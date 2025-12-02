dd55d45a49d8e0ea6fd139813f5dab7dce9f1a0b
Renato Aquino <renato.silva.aquino12@gmail.com>
2025-12-02 05:00:33 -0300

    feat: removing unecessary menssages

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { bot, sendProcessProduct, chatId } from "./bot.js";
 
 // === EXECUTAR ===
 async function iniciar() {
-  bot.sendMessage(chatId, "🔍 Iniciando scraping...");
-
   const produtos = await scrapeML({
     url: "https://lista.mercadolivre.com.br/informatica/tablets-acessorios/tablets/apple/ipad/usado/ipados/ipad-10_OrderId_PRICE_PriceRange_1500-3400_NoIndex_True",
   });
